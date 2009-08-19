e33bc9f0b54e0f8a9d3604f2ec98cf8d85540828
Dmitry Vasiliev <dima@hlabs.org>
2009-08-19 22:11:46 +0400

    Added options to show/hide number, size and type

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,1 +1,4 @@
 pref("extensions.videosniffer.videolimit", 20);
+pref("extensions.videosniffer.shownumber", true);
+pref("extensions.videosniffer.showsize", true);
+pref("extensions.videosniffer.showtype", true);
