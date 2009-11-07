165fd7d44b9f6b416adf8005d811a25646e9f783
Dmitry Vasiliev <dima@hlabs.org>
2009-11-07 14:02:52 +0300

    Added preference observer

--- chrome/content/videosniffer.js
+++ chrome/content/videosniffer.js
@@ -23,16 +23,28 @@ var VideoSniffer = {
     counter: 0,
     videos: new Object(),
 
+    /* On load callback */
     onLoad: function()
     {
+        /* HTTP observer */
         this.getObserver().addObserver(VideoSniffer,
             "http-on-examine-response", false);
+
+        /* Preference observer */
+        var branch = this.getPrefs();
+        branch.QueryInterface(Components.interfaces.nsIPrefBranch2);
+        branch.addObserver("", this, false);
     },
 
+    /* On unload callback */
     onUnload: function()
     {
+        /* HTTP observer */
         this.getObserver().removeObserver(VideoSniffer,
             "http-on-examine-response");
+
+        /* Preference observer */
+        this.getPrefs().removeObserver("", this);
     },
 
     getObserver: function()
@@ -109,8 +121,18 @@ var VideoSniffer = {
 
     observe: function(subject, topic, data)
     {
-        if (topic != "http-on-examine-response")
-            return
+        switch (topic) {
+            case "http-on-examine-response":
+                this.examineResponse(subject, data);
+                break;
+            case "nsPref:changed":
+                this.preferencesChanged(subject, data);
+                break;
+        }
+    },
+
+    examineResponse: function(subject, data)
+    {
         var channel = subject.QueryInterface(
             Components.interfaces.nsIHttpChannel);
         if (channel.requestSucceeded) {
@@ -118,6 +140,10 @@ var VideoSniffer = {
             if (uri_info.isVideo())
                 this.addURL(uri_info);
         }
+    },
+
+    preferencesChanged: function(subject, data)
+    {
     }
 }
 
