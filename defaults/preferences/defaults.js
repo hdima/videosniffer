4bec73a1ed5d90a50093e187149149f04206f4db
Dmitry Vasiliev <dima@hlabs.org>
2009-11-08 14:51:45 +0300

    Added option to ignore possible AD videos

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,4 +1,5 @@
 pref("extensions.videosniffer.videolimit", 20);
+pref("extensions.videosniffer.ignorejunk", true);
 pref("extensions.videosniffer.shownumber", true);
 pref("extensions.videosniffer.showsize", true);
 pref("extensions.videosniffer.showtype", true);
